c44f0a884ee9628eac14f6241afc2a25e793093e
xLeoArizax <leonardoariza201@gmail.com>
2024-04-14 19:27:18 -0500

    Se agrega el Proyecto

---
+++ PAGINA WEB/fronted/foto/fot.py
@@ -0,0 +1,13 @@
+from dash import html # type: ignore
+import dash_bootstrap_components as dbc # type: ignore
+
+picture = dbc.Container([  
+       html.Br(),
+       html.Br(),
+       html.Br(),
+       html.H1('foto'),
+       html.Br(),
+       html.Br(),
+       html.Br(),
+
+]),
